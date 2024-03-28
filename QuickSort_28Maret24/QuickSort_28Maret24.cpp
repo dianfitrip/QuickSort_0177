3c0ebd88985a39132a778de74dbbe78cf52883d1
dianfitrip <dianfitri683@gmail.com>
2024-03-28 09:55:16 +0700

    prosedur input data

--- QuickSort_28Maret24/QuickSort_28Maret24.cpp
+++ QuickSort_28Maret24/QuickSort_28Maret24.cpp
@@ -7,3 +7,17 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input()
+{
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+}
