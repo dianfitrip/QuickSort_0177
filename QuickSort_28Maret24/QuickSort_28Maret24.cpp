0bd666f0f73c39f332929f4d0906364d72338c6b
dianfitrip <dianfitri683@gmail.com>
2024-03-28 10:47:36 +0700

    memanggil input, quicksort, display dan membuat do...

--- QuickSort_28Maret24/QuickSort_28Maret24.cpp
+++ QuickSort_28Maret24/QuickSort_28Maret24.cpp
@@ -94,3 +94,20 @@ void display()
 	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
 	cout << "Number of data movements: " << mov_count << endl;
 }
+
+int main()
+{
+	char ch;
+
+	do
+	{
+		input();
+		q_sort(0, n - 1);
+		display();
+		cout << "\n\nDo you want to continue? (y/n): ";
+		cin >> ch;
+		if (ch == 'n' || ch == 'N')
+			break;
+
+	}
+}
